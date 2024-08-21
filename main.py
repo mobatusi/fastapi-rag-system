078d5d1ad63b337c035fc512ce6d791c94b96d1a
mobatusi <mobatusi@gmail.com>
2024-08-20 21:32:33 -0400

    update main.py to improve the document upload so that its more robust and secure

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from pydantic import BaseModel
 import os
 from openai import OpenAI
+import shutil
 
 
 app = FastAPI()
@@ -14,21 +15,35 @@ class AskQuestionModel(BaseModel):
     question: str
 
 @app.get("/")
-def read_root():
-    return {"Hello": "Please upload your file"}
+def root():
+    return "Hello RAG fellow!"
 
 @app.post("/uploadfile/")
-async def uploadfile(file: UploadFile):
+async def uploadfile(file: UploadFile = File(...)):
+    # Define allowed file extensions
+    allowed_extensions = ["txt", "pdf"]
+
+    # Check if the file extension is allowed
+    file_extension = file.filename.split(".")[-1].lower()
+
+    if file_extension not in allowed_extensions:
+        raise HTTPException(status_code=400, detail="File type not allowed. Only TXT and PDF files are accepted.")
+    
     folder = "documents"
     current_directory = os.getcwd()
     print(current_directory)
+
     try:
+        # Ensure the directory exists
         if not os.path.exists(folder):
             os.makedirs(folder)
+
         file_path = os.path.join(current_directory, folder, file.filename)
-        with open(file_path, "wb") as f:
-            f.write(file.file.read())
-            return {"info": "File saved", "filename": file.filename}
+
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        
+        return {"info": "File saved", "filename": file.filename}
     except Exception as e:
         return {"message": e.args}
         
@@ -46,4 +61,7 @@ async def ask_question(data: AskQuestionModel):
                 }
             ]
         )
-     
+        answer = completion.choices[0].message.content
+        return {"answer": answer}
+    except Exception as e:
+        return {"message": e.args}
